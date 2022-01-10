61e1f56f4858e5e5f5278cd4968b4ce1bc20ad08
Bogdan Silvasan <bogdan.silve@gmail.com>
2022-01-10 12:16:28 +0200

    Added RSquared error

--- linear_regression.py
+++ linear_regression.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).T
 y = np.array([4204.11, 4297.50, 4395.26, 4522.68, 4307.54, 4605.38, 4567.00, 4766.18, 4700.58, 4704.01]).T
+yCopy = np.copy(y)
 
 plt.scatter(x, y)
 
@@ -66,6 +67,17 @@ for k in range(n):
 def getError(initVec, finalVec):
     return np.sqrt(sum((finalVec[0:initVec.size] - initVec) ** 2)) / (initVec.size - 2)
 
+def getRSquaredError(initVec, finalVec):
+    meanY = np.zeros(initVec.size) # Vector de comparatie: are media lui initVec pe fiecare pozitie
+    for i in range(initVec.size):
+        meanY[i] = np.mean(initVec)
+    
+    err = sum((finalVec[0:initVec.size] - initVec) ** 2) # Eroarea simpla
+    meanErr = sum((meanY - initVec) ** 2) # Eroarea medie
+
+    return 1 - (err / meanErr)
+    
+
 c = Utris(R[0:n,:], y[0:n])
 print(c)
 
@@ -77,6 +89,7 @@ for val in range(20):
 
 plt.plot(range(20), newY)
 
-print(getError(y, newY))
+print("Simple error: " + str(getError(yCopy, newY)))
+print("R squared error: " + str(getRSquaredError(yCopy, newY)))
 
 plt.show()
