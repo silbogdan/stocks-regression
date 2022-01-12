33e7d7fdb9e9ac6dcf5a606535845fc7c73d6cc8
Bogdan Silvasan <bogdan.silve@gmail.com>
2022-01-12 08:54:36 +0200

    Initialized web app and added logos

---
+++ app/client/src/App.js
@@ -0,0 +1,9 @@
+import MainScreen from './screens/MainScreen';
+
+function App() {
+  return (
+    <MainScreen />
+  );
+}
+
+export default App;
---
+++ app/client/src/screens/MainScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainScreen = () => {
+    return (
+        <div>
+            This is the main screen, lol
+        </div>
+    )
+}
+
+export default MainScreen
